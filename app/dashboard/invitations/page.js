de642e4b2b721d39192bfaaa1586b7caf39be11c
rishabhdev2700 <rishavkumar2700@gmail.com>
2024-10-16 00:12:23 +0530

    fixed invitation sending

--- app/dashboard/invitations/page.js
+++ app/dashboard/invitations/page.js
@@ -1,55 +1,15 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { useState, useEffect } from "react";
+import InvitationList from "@/components/custom/invitation/invitation-list";
+import { fetchMyInvitations } from "@/lib/getData";
+import { getSession } from "@/lib/session";
+import { Invitation } from "@/models/invitation";
 
 
 
-export default function Page() {
-  const [invitations, setInvitations] = useState(fakeData);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchInvitations = async () => {
-      try {
-        // const data = await fetchProjectsByEmail('your_email'); // Replace with your actual email
-        // setInvitations(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
-
-    fetchInvitations();
-  }, []);
-
-  const handleAcceptInvitation = (projectId) => {
-    // Implement your logic to accept the invitation (e.g., update the database)
-    console.log('Accepting invitation for project:', projectId);
-  };
-
-  const handleRejectInvitation = (projectId) => {
-    // Implement your logic to reject the invitation (e.g., update the database)
-    console.log('Rejecting invitation for project:', projectId);
-  };
-
+export default async function Page() {
+  const user = await getSession()
+  console.log("user:",user)
+  const invitations = await fetchMyInvitations(user.email)
   return (
-    <div className="mx-auto md:w-5/6 xl:w-2/3">
-      <h2 className="text-3xl my-4 pl-8 underline">Invitations</h2>
-      {loading && <p className="text-center text-gray-500">Loading invitations...</p>}
-      {invitations.length === 0 && !loading && <p className="text-center text-gray-500">No invitations found.</p>}
-      <ul className="mt-4">
-        {invitations.map((invitation) => (
-          <li key={invitation._id} className="mb-4 mx-4 border px-4 py-2 rounded-xl shadow-md shadow-black/10 flex justify-between items-center">
-            <h2 className="text-xl font-bold">{invitation.title}</h2>
-            <p className="text-gray-700">Role: {invitation.team[0].role}</p>
-            <div className="flex items-center space-x-4 mt-2">
-              <Button className="bg-green-400 hover:bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => handleAcceptInvitation(invitation._id)}>Accept</Button>
-              <Button className="bg-red-400 hover:bg-red-500 text-white px-4 py-2 rounded-md" onClick={() => handleRejectInvitation(invitation._id)}>Reject</Button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <InvitationList invitations={invitations}></InvitationList>
   );
 };
--- app/dashboard/projects/[projectID]/page.js
+++ app/dashboard/projects/[projectID]/page.js
@@ -1,71 +1,83 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { fetchSingleProject } from "@/lib/getData"
-import { getSession } from "@/lib/session"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Pencil1Icon } from "@radix-ui/react-icons"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import TaskOverview from "@/components/custom/task-overview"
-import { status, statusColor } from "@/lib/constants"
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import InvitationForm from "@/components/custom/invitation-form"
-import TaskForm from "@/components/custom/task-form"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { fetchSingleProject } from "@/lib/getData";
+import { getSession } from "@/lib/session";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import TaskOverview from "@/components/custom/task-overview";
+import { status, statusColor } from "@/lib/constants";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import InvitationForm from "@/components/custom/invitation/invitation-form";
+import TaskForm from "@/components/custom/task-form";
 
 export default async function Page({ params }) {
-    let user = await getSession()
-    const { project, tasks } = await fetchSingleProject(user.userID, params.projectID)
-    return (
-        <Card className="m-2 p-2 w-full lg:w-5/6 xl:w-2/3 mx-auto shadow-md shadow-black/20 dark:bg-neutral-900">
-            <CardHeader className=" flex flex-row justify-between">
-                <div className="text-3xl font-bold">{project.title}</div>
-                <div className="inline"><Link href={`/dashboard/projects/${project._id}/edit`}><Button size="icon" className="bg-purple-500 hover:bg-purple-600"><Pencil1Icon /></Button></Link></div>
-            </CardHeader>
-            <CardContent >
-                <div className="grid lg:grid-cols-2 lg:gap-12">
+  let user = await getSession();
+  const { project, tasks } = await fetchSingleProject(
+    user.userID,
+    params.projectID
+  );
+  return (
+    <Card className="m-2 p-2 w-full lg:w-5/6 xl:w-2/3 mx-auto shadow-md shadow-black/20 dark:bg-neutral-900">
+      <CardHeader className=" flex flex-row justify-between">
+        <div className="text-3xl font-bold">{project.title}</div>
+        <div className="inline">
+          <Link href={`/dashboard/projects/${project._id}/edit`}>
+            <Button size="icon" className="bg-purple-500 hover:bg-purple-600">
+              <Pencil1Icon />
+            </Button>
+          </Link>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="grid lg:grid-cols-2 lg:gap-12">
+          <img
+            alt="project-img"
+            className="rounded-lg shadow-md shadow-black/20 lg:h-96 lg:mx-auto lg:w-full object-cover"
+            src="https://images.pexels.com/photos/7911758/pexels-photo-7911758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
 
-                    <img alt="project-img" className="rounded-lg shadow-md shadow-black/20 lg:h-96 lg:mx-auto lg:w-full object-cover" src="https://images.pexels.com/photos/7911758/pexels-photo-7911758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-
-                    <div className="py-4">
-                        <CardDescription>
-                            {project.description}
-                        </CardDescription>
-                        <div>
-                            {project.status}
-                        </div>
-                    </div>
-                </div>
-                <div className="grid lg:grid-cols-2 lg:gap-12">
-
-                    <Card className="my-4">
-                        <div className="flex justify-between items-center p-2">
-                            <CardTitle>Team</CardTitle>
-                            <Dialog>
-                                <DialogTrigger asChild>
-                                    <Button className="bg-indigo-400 hover:bg-indigo-800" >
-                                        <Pencil1Icon />
-                                    </Button>
-                                </DialogTrigger>
-                                <DialogContent>
-                                    <DialogHeader>
-                                        <DialogTitle>Fill the following to invite</DialogTitle>
-                                        <DialogDescription>
-                                            Enter the email and select the role
-                                        </DialogDescription>
-                                        <InvitationForm projectID={params.projectID} />
-                                    </DialogHeader>
-                                </DialogContent>
-                            </Dialog>
-
-                        </div>
-                        <ScrollArea>
-                            {/* {project.team.map((m, i) => {
+          <div className="py-4">
+            <CardDescription>{project.description}</CardDescription>
+            <div>{project.status}</div>
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-2 lg:gap-12">
+          <Card className="my-4">
+            <div className="flex justify-between items-center p-2">
+              <CardTitle>Team</CardTitle>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className="bg-indigo-400 hover:bg-indigo-800">
+                    <Pencil1Icon />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Fill the following to invite</DialogTitle>
+                    <DialogDescription>
+                      Enter the email and select the role
+                    </DialogDescription>
+                    <InvitationForm projectID={params.projectID} />
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <ScrollArea>
+              {/* {project.team.map((m, i) => {
                                 return <Card key={i} className="p-2 mx-2 my-1 hover:bg-black/10 flex justify-between items-center text-sm font-sm">
                                     <div>
                                         {m.email}
@@ -75,31 +87,32 @@ export default async function Page({ params }) {
                                     </div>
                                 </Card>
                             })} */}
+            </ScrollArea>
+          </Card>
 
-                        </ScrollArea>
-                    </Card>
-
-                    <Card className="my-4">
-                        <div className="flex justify-between items-center p-2">
-                            <CardTitle>Tasks</CardTitle>
-                            <TaskForm projectID={params.projectID} team={JSON.parse(JSON.stringify(project.team))} />
-                        </div>
-                        <div className="px-2">
-                            {
-                                tasks.map((t) => {
-                                    return (
-                                        <TaskOverview projectID={params.projectID} task={t} text={t.status} color={`bg-${statusColor(t.status)}-500`} key={t.id} />
-
-                                    )
-                                })}
-                        </div>
-
-                    </Card>
-
-                </div>
-                {/* <KanbanBoard tasks={[...tasks]} /> */}
-            </CardContent>
-        </Card>
-
-    )
+          <Card className="my-4">
+            <div className="flex justify-between items-center p-2">
+              <CardTitle>Tasks</CardTitle>
+              <TaskForm
+                projectID={params.projectID}
+                team={JSON.parse(JSON.stringify(project.team))}
+              />
+            </div>
+            <div className="px-2">
+              {tasks.map((t) => (
+                <TaskOverview
+                  projectID={params.projectID}
+                  task={t}
+                  text={t.status}
+                  color={`bg-${statusColor(t.status)}-500`}
+                  key={t.id}
+                />
+              ))}
+            </div>
+          </Card>
+        </div>
+        {/* <KanbanBoard tasks={[...tasks]} /> */}
+      </CardContent>
+    </Card>
+  );
 }
